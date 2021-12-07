9f695bad82df43794165759a958369856b239740
paulfgraham <paul@smartcasual.io>
2021-12-07 13:35:45 +0000

    Update index.js

--- src/models/index.js
+++ src/models/index.js
@@ -1,10 +1,6 @@
 // Local imports
 import ComponentTypes from './ComponentTypes';
-import FormTypes from './FormTypes';
-import HubFormats from './HubFormats';
 
 export {
   ComponentTypes,
-  FormTypes,
-  HubFormats
 };
