7538721a682b3c92709af4c11111c2c70dc8775a
paulfgraham <paul@smartcasual.io>
2021-12-22 14:16:28 +0000

    Add RowAction component
    Added the `RowAction` component, which is used to display the "Change" link, along with any hidden text for screen readers, on the *Check your answers* screen.
    
    This also includes a helper function and unit tests.

---
+++ src/components/SummaryList/helpers/getRowActionAttributes.test.js
@@ -0,0 +1,73 @@
+import getRowActionAttributes from './getRowActionAttributes';
+
+describe('components', () => {
+
+  describe('SummaryList', () => {
+
+    describe('helpers', () => {
+
+      describe('getRowActionAttributes', () => {
+
+        it('should handle a null row', () => {
+          expect(getRowActionAttributes(null)).toEqual({});
+        });
+
+        it('should handle a row without an action', () => {
+          expect(getRowActionAttributes({})).toEqual({});
+        });
+
+        it('should handle a row with an empty action', () => {
+          const ROW = {
+            action: {}
+          };
+          expect(getRowActionAttributes(ROW)).toEqual({});
+        });
+
+        it('should handle a row with an href', () => {
+          const HREF = 'http://alpha.homeoffice.gov.uk';
+          const ROW = {
+            action: { href: HREF }
+          };
+          expect(getRowActionAttributes(ROW)).toEqual({ href: HREF });
+        });
+
+        it('should handle a row with an onAction function', () => {
+          const ON_ACTION_CALLS = [];
+          const ON_ACTION = (row) => {
+            ON_ACTION_CALLS.push(row);
+          };
+          const ROW = {
+            action: { onAction: ON_ACTION }
+          };
+          const ATTRS = getRowActionAttributes(ROW);
+          expect(ATTRS.onClick).toBeDefined();
+          expect(ATTRS.href).not.toBeDefined();
+          ATTRS.onClick();
+          expect(ON_ACTION_CALLS.length).toEqual(1);
+          expect(ON_ACTION_CALLS[0]).toEqual(ROW);
+        });
+
+        it('should favour onAction over href', () => {
+          const ON_ACTION_CALLS = [];
+          const ON_ACTION = (row) => {
+            ON_ACTION_CALLS.push(row);
+          };
+          const HREF = 'http://alpha.homeoffice.gov.uk';
+          const ROW = {
+            action: { href: HREF, onAction: ON_ACTION }
+          };
+          const ATTRS = getRowActionAttributes(ROW);
+          expect(ATTRS.onClick).toBeDefined();
+          expect(ATTRS.href).not.toBeDefined();
+          ATTRS.onClick();
+          expect(ON_ACTION_CALLS.length).toEqual(1);
+          expect(ON_ACTION_CALLS[0]).toEqual(ROW);
+        });
+
+      });
+
+    });
+
+  });
+
+});
