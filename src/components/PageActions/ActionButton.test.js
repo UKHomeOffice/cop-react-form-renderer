e37b34fd2643993955b35328e5b12abde656d379
paulfgraham <paul@smartcasual.io>
2021-12-22 12:53:53 +0000

    Add PageActions
    Added the `PageActions` and accompanying `ActionButton` components, along with unit tests.

---
+++ src/components/PageActions/ActionButton.test.js
@@ -0,0 +1,68 @@
+// Global imports
+import { fireEvent, render } from '@testing-library/react';
+import React from 'react';
+
+// Local imports
+import ActionButton, { DEFAULT_ACTIONS, DEFAULT_LABEL } from './ActionButton';
+
+describe('components', () => {
+
+  describe('PageActions.ActionButton', () => {
+    const ON_ACTION_CALLS = [];
+    const ON_ACTION = (action) => {
+      ON_ACTION_CALLS.push(action);
+    };
+    
+    beforeEach(() => {
+      ON_ACTION_CALLS.length = 0;
+    });
+
+    it('should handle a null action', async () => {
+      const ACTION = null;
+      const { container } = render(
+        <ActionButton action={ACTION} onAction={ON_ACTION} />
+      );
+      expect(container.childNodes.length).toEqual(0);
+    });
+
+    it('should handle an unknown action identifier', async () => {
+      const ACTION = 'unknown';
+      const { container } = render(
+        <ActionButton action={ACTION} onAction={ON_ACTION} />
+      );
+      expect(container.childNodes.length).toEqual(0);
+    });
+
+    it('should appropriately display a submit action', async () => {
+      const ACTION = 'submit';
+      const { container } = render(
+        <ActionButton action={ACTION} onAction={ON_ACTION} />
+      );
+      const button = container.childNodes[0];
+      expect(button.tagName).toEqual('BUTTON');
+      expect(button.textContent).toEqual(DEFAULT_LABEL);
+
+      // Click the button and make sure it fires the onAction handler.
+      fireEvent.click(button, {});
+      expect(ON_ACTION_CALLS.length).toEqual(1);
+      expect(ON_ACTION_CALLS[0]).toEqual(DEFAULT_ACTIONS.submit);
+    });
+
+    it('should appropriately display a custom action', async () => {
+      const ACTION = { type: 'navigate', url: '/alpha', label: 'Alpha' };
+      const { container } = render(
+        <ActionButton action={ACTION} onAction={ON_ACTION} />
+      );
+      const button = container.childNodes[0];
+      expect(button.tagName).toEqual('BUTTON');
+      expect(button.textContent).toEqual(ACTION.label);
+
+      // Click the button and make sure it fires the onAction handler.
+      fireEvent.click(button, {});
+      expect(ON_ACTION_CALLS.length).toEqual(1);
+      expect(ON_ACTION_CALLS[0]).toEqual(ACTION);
+    });
+
+  });
+
+});
