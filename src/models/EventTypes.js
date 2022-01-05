2d383de0d3c4de37e39731a611a97d163fccf8b6
paulfgraham <paul@smartcasual.io>
2022-01-05 17:28:18 +0000

    Add EventTypes

---
+++ src/models/EventTypes.js
@@ -0,0 +1,9 @@
+const TYPE_BLUR = 'blur';
+const TYPE_SUBMIT = 'submit';
+
+const EventTypes = {
+  BLUR: TYPE_BLUR,
+  SUBMIT: TYPE_SUBMIT
+};
+
+export default EventTypes;
--- src/models/index.js
+++ src/models/index.js
@@ -1,10 +1,12 @@
 // Local imports
 import ComponentTypes from './ComponentTypes';
+import EventTypes from './EventTypes';
 import FormTypes from './FormTypes';
 import HubFormats from './HubFormats';
 
 export {
   ComponentTypes,
+  EventTypes,
   FormTypes,
   HubFormats
 };
