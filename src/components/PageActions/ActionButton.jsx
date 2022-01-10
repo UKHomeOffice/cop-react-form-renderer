4f82539010b1aa79e34b7090f288400e11e5c1a8
paulfgraham <paul@smartcasual.io>
2022-01-10 17:03:55 +0000

    Support addition action button types

--- src/components/PageActions/ActionButton.jsx
+++ src/components/PageActions/ActionButton.jsx
@@ -3,21 +3,26 @@ import { Button } from '@ukhomeoffice/cop-react-components';
 import PropTypes from 'prop-types';
 import React from 'react';
 
+// Local imports
+import { PageAction } from '../../models';
+
 export const DEFAULT_LABEL = 'Continue';
-export const DEFAULT_ACTIONS = {
-  submit: { type: 'submit', validate: true }
-};
 const ActionButton = ({
   action: _action,
   onAction,
   ...attrs
 }) => {
-  const action = typeof(_action) === 'string' ? DEFAULT_ACTIONS[_action] : _action;
+  const action = typeof(_action) === 'string' ? PageAction.DEFAULTS[_action] : _action;
   if (!action) {
     return null;
   }
   return (
-    <Button {...attrs} className={action.className} classModifiers={action.classModifiers} onClick={() => onAction(action)}>
+    <Button
+      {...attrs}
+      className={action.className}
+      classModifiers={action.classModifiers}
+      start={action.start || false}
+      onClick={() => onAction(action)}>
       {action.label || DEFAULT_LABEL}
     </Button>
   );
