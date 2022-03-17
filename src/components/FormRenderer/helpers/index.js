bb9d3fb96811802fd2e969e91f394297d7be6400
paulfgraham <paul@smartcasual.io>
2022-03-17 12:01:25 +0000

    Save and use form status
    Added a utility function to set up the status of the form, which is added to the payload at the point of saving in a `formStatus` variable.
    
    Correspondingly, when the form initially loads, the renderer will navigate to the appropriate page if it encounters a `data.formStatus` object. Otherwise, it will default to the "first" page, as determined by the form type.

--- src/components/FormRenderer/helpers/index.js
+++ src/components/FormRenderer/helpers/index.js
@@ -3,11 +3,13 @@ import canActionProceed from './canActionProceed';
 import canCYASubmit from './canCYASubmit';
 import getFormState from './getFormState';
 import getNextPageId from './getNextPageId';
+import getSubmissionStatus from './getSubmissionStatus';
 
 export const helpers = {
   canActionProceed,
   canCYASubmit,
   getFormState,
-  getNextPageId
+  getNextPageId,
+  getSubmissionStatus
 };
 export default helpers;
