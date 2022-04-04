724e2689902b7be0cd4ce29993050fc20aa6eae3
keenan218 <keenan.bratley@digital.homeoffice.gov.uk>
2022-04-04 11:55:37 +0100

    Added a read-only view for SummaryList

--- src/components/SummaryList/SummaryList.jsx
+++ src/components/SummaryList/SummaryList.jsx
@@ -12,6 +12,8 @@ import './SummaryList.scss';
 export const DEFAULT_CLASS = 'govuk-summary-list';
 const SummaryList = ({
   rows,
+  noAction,
+  invertWeight,
   classBlock,
   classModifiers,
   className,
@@ -19,40 +21,63 @@ const SummaryList = ({
 }) => {
   const classes = Utils.classBuilder(classBlock, classModifiers, className);
   return (
-    <dl {...attrs } className={classes()}>
-      {rows.map(row => (
-        <div key={`${row.pageId}_${row.fieldId}`} className={classes('row')}>
-          <dt className={classes('key')}>{row.key}</dt>
-          <dd className={classes('value')}>{row.value}</dd>
-          <dd className={classes('actions')}>
-            {row.action && <RowAction row={row} />}
-          </dd>
-        </div>
-      ))}
+    <dl {...attrs} className={classes()}>
+      {rows.map((row) =>
+        invertWeight ? (
+          <div key={`${row.pageId}_${row.fieldId}`} className={classes('row')}>
+            <dt className={classes('key caseview__key')}>{row.key}</dt>
+            <dd className={classes('value caseview__value')}>{row.value}</dd>
+            {!noAction && (
+              <dd className={classes('actions')}>
+                {row.action && <RowAction row={row} />}
+              </dd>
+            )}
+          </div>
+        ) : (
+          <div key={`${row.pageId}_${row.fieldId}`} className={classes('row')}>
+            <dt className={classes('key')}>{row.key}</dt>
+            <dd className={classes('value')}>{row.value}</dd>
+            {!noAction && (
+              <dd className={classes('actions')}>
+                {row.action && <RowAction row={row} />}
+              </dd>
+            )}
+          </div>
+        )
+      )}
     </dl>
   );
 };
 
 SummaryList.propTypes = {
-  rows: PropTypes.arrayOf(PropTypes.shape({
-    pageId: PropTypes.string.isRequired,
-    fieldId: PropTypes.string.isRequired,
-    key: PropTypes.string.isRequired,
-    value: PropTypes.any,
-    action: PropTypes.shape({
-      page: PropTypes.string,
-      label: PropTypes.string,
-      aria_suffix: PropTypes.string,
-      onAction: PropTypes.func
+  rows: PropTypes.arrayOf(
+    PropTypes.shape({
+      pageId: PropTypes.string.isRequired,
+      fieldId: PropTypes.string.isRequired,
+      key: PropTypes.string.isRequired,
+      value: PropTypes.any,
+      action: PropTypes.shape({
+        page: PropTypes.string,
+        label: PropTypes.string,
+        aria_suffix: PropTypes.string,
+        onAction: PropTypes.func
+      })
     })
-  })).isRequired,
+  ).isRequired,
   classBlock: PropTypes.string,
-  classModifiers: PropTypes.oneOfType([PropTypes.string, PropTypes.arrayOf(PropTypes.string)]),
-  className: PropTypes.string
+  classModifiers: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.arrayOf(PropTypes.string)
+  ]),
+  className: PropTypes.string,
+  noAction: PropTypes.bool,
+  invertWeight: PropTypes.bool
 };
 
 SummaryList.defaultProps = {
-  classBlock: DEFAULT_CLASS
+  classBlock: DEFAULT_CLASS,
+  noAction: false,
+  invertWeight: false
 };
 
 export default SummaryList;
--- src/components/SummaryList/SummaryList.test.js
+++ src/components/SummaryList/SummaryList.test.js
@@ -6,9 +6,7 @@ import React from 'react';
 import SummaryList, { DEFAULT_CLASS } from './SummaryList';
 
 describe('components', () => {
-
   describe('SummaryList', () => {
-
     const checkSummaryList = (container, id) => {
       const summaryList = getByTestId(container, id);
       expect(summaryList.tagName).toEqual('DL');
@@ -20,14 +18,27 @@ describe('components', () => {
       const row = summaryList.childNodes[index];
       expect(row.tagName).toEqual('DIV');
       expect(row.classList).toContain(`${DEFAULT_CLASS}__row`);
-      const [ key, value, actions ] = row.childNodes;
+      const [key, value, actions] = row.childNodes;
       expect(key.tagName).toEqual('DT');
       expect(key.classList).toContain(`${DEFAULT_CLASS}__key`);
       expect(value.tagName).toEqual('DD');
       expect(value.classList).toContain(`${DEFAULT_CLASS}__value`);
       expect(actions.tagName).toEqual('DD');
       expect(actions.classList).toContain(`${DEFAULT_CLASS}__actions`);
-      return [ key, value, actions ];
+      return [key, value, actions];
+    };
+
+    const checkRowNoActions = (summaryList, index) => {
+      const row = summaryList.childNodes[index];
+      expect(row.tagName).toEqual('DIV');
+      expect(row.classList).toContain(`${DEFAULT_CLASS}__row`);
+      const [key, value, actions] = row.childNodes;
+      expect(key.tagName).toEqual('DT');
+      expect(key.classList).toContain(`${DEFAULT_CLASS}__key`);
+      expect(value.tagName).toEqual('DD');
+      expect(value.classList).toContain(`${DEFAULT_CLASS}__value`);
+
+      return [key, value, actions];
     };
 
     it('should handle an empty rows array', () => {
@@ -52,7 +63,7 @@ describe('components', () => {
       const summaryList = checkSummaryList(container, ID);
       expect(summaryList.childNodes.length).toEqual(ROWS.length);
       ROWS.forEach((row, index) => {
-        const [ key, value, actions ] = checkRow(summaryList, index);
+        const [key, value, actions] = checkRow(summaryList, index);
         expect(key.textContent).toEqual(row.key);
         expect(value.textContent).toEqual(row.value);
         expect(actions.childNodes.length).toEqual(0);
@@ -63,8 +74,18 @@ describe('components', () => {
       const ID = 'test-id';
       const VALUES = ['Alpha component value', 'Bravo component value'];
       const ROWS = [
-        { pageId: 'p1', fieldId: 'a', key: 'Alpha', value: <div>{VALUES[0]}</div> },
-        { pageId: 'p2', fieldId: 'b', key: 'Bravo', value: <div>{VALUES[1]}</div> }
+        {
+          pageId: 'p1',
+          fieldId: 'a',
+          key: 'Alpha',
+          value: <div>{VALUES[0]}</div>
+        },
+        {
+          pageId: 'p2',
+          fieldId: 'b',
+          key: 'Bravo',
+          value: <div>{VALUES[1]}</div>
+        }
       ];
       const { container } = render(
         <SummaryList data-testid={ID} rows={ROWS} />
@@ -72,7 +93,7 @@ describe('components', () => {
       const summaryList = checkSummaryList(container, ID);
       expect(summaryList.childNodes.length).toEqual(ROWS.length);
       ROWS.forEach((row, index) => {
-        const [ key, value, actions ] = checkRow(summaryList, index);
+        const [key, value, actions] = checkRow(summaryList, index);
         expect(key.textContent).toEqual(row.key);
         expect(value.childNodes.length).toEqual(1);
         const valueDiv = value.childNodes[0];
@@ -89,8 +110,20 @@ describe('components', () => {
         ON_ACTION_CALLS.push(row);
       };
       const ROWS = [
-        { pageId: 'p1', fieldId: 'a', key: 'Alpha', value: 'Alpha value', action: { label: 'Change A', onAction: ON_ACTION } },
-        { pageId: 'p2', fieldId: 'b', key: 'Bravo', value: 'Bravo value', action: { label: 'Change B', onAction: ON_ACTION } }
+        {
+          pageId: 'p1',
+          fieldId: 'a',
+          key: 'Alpha',
+          value: 'Alpha value',
+          action: { label: 'Change A', onAction: ON_ACTION }
+        },
+        {
+          pageId: 'p2',
+          fieldId: 'b',
+          key: 'Bravo',
+          value: 'Bravo value',
+          action: { label: 'Change B', onAction: ON_ACTION }
+        }
       ];
       const { container } = render(
         <SummaryList data-testid={ID} rows={ROWS} />
@@ -98,7 +131,7 @@ describe('components', () => {
       const summaryList = checkSummaryList(container, ID);
       expect(summaryList.childNodes.length).toEqual(ROWS.length);
       ROWS.forEach((row, index) => {
-        const [ key, value, actions ] = checkRow(summaryList, index);
+        const [key, value, actions] = checkRow(summaryList, index);
         expect(key.textContent).toEqual(row.key);
         expect(value.textContent).toEqual(row.value);
         const a = actions.childNodes[0];
@@ -109,6 +142,77 @@ describe('components', () => {
       });
     });
 
-  });
+    it('should handle rows with component values and actions set to hidden', () => {
+      const ID = 'test-id';
+      const VALUES = ['Alpha component value', 'Bravo component value'];
+      const ROWS = [
+        {
+          pageId: 'p1',
+          fieldId: 'a',
+          key: 'Alpha',
+          value: <div>{VALUES[0]}</div>
+        },
+        {
+          pageId: 'p2',
+          fieldId: 'b',
+          key: 'Bravo',
+          value: <div>{VALUES[1]}</div>
+        }
+      ];
+      const { container } = render(
+        <SummaryList data-testid={ID} rows={ROWS} noAction={true} />
+      );
+      const summaryList = checkSummaryList(container, ID);
+      expect(summaryList.childNodes.length).toEqual(ROWS.length);
+      ROWS.forEach((row, index) => {
+        const [key, value] = checkRowNoActions(summaryList, index);
+        expect(key.textContent).toEqual(row.key);
+        expect(value.childNodes.length).toEqual(1);
+        const valueDiv = value.childNodes[0];
+        expect(valueDiv.tagName).toEqual('DIV');
+        expect(valueDiv.textContent).toEqual(VALUES[index]);
+      });
+    });
 
+    it('should handle rows with component values and font weights set to inverted', () => {
+      const ID = 'test-id';
+      const ON_ACTION_CALLS = [];
+      const ON_ACTION = (row) => {
+        ON_ACTION_CALLS.push(row);
+      };
+      const ROWS = [
+        {
+          pageId: 'p1',
+          fieldId: 'a',
+          key: 'Alpha',
+          value: 'Alpha value',
+          action: { label: 'Change A', onAction: ON_ACTION }
+        },
+        {
+          pageId: 'p2',
+          fieldId: 'b',
+          key: 'Bravo',
+          value: 'Bravo value',
+          action: { label: 'Change B', onAction: ON_ACTION }
+        }
+      ];
+      const { container } = render(
+        <SummaryList data-testid={ID} rows={ROWS} invertWeight={true} />
+      );
+      const summaryList = checkSummaryList(container, ID);
+      expect(summaryList.childNodes.length).toEqual(ROWS.length);
+      ROWS.forEach((row, index) => {
+        const [key, value, actions] = checkRow(summaryList, index);
+        expect(key.textContent).toEqual(row.key);
+        expect(key.classList).toContain('caseview__key');
+        expect(value.textContent).toEqual(row.value);
+        expect(value.classList).toContain('caseview__value');
+        const a = actions.childNodes[0];
+        expect(a.textContent).toEqual(row.action.label);
+        fireEvent.click(a, {});
+        expect(ON_ACTION_CALLS.length).toEqual(index + 1);
+        expect(ON_ACTION_CALLS[index]).toEqual(row);
+      });
+    });
+  });
 });
