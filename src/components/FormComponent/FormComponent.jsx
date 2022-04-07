b94f3f9630c060cb6b4bcafbd01c3f3a8707a9cd
paulfgraham <paul@smartcasual.io>
2022-04-07 15:48:19 +0100

    Add hook for overriding component
    Added a hook for overriding components. The override function is passed the `wrap` parameter where a value of `false` means it _shouldn't_ be wrapped and a value of `true` means the function should decide whether or not it's should be within a `FormGroup` and wrap it if necessary.

--- src/components/FormComponent/FormComponent.jsx
+++ src/components/FormComponent/FormComponent.jsx
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import { useEffect, useState } from 'react';
 
 // Local imports
+import useHooks from '../../hooks/useHooks';
 import useRefData, { STATUS_COMPLETE } from '../../hooks/useRefData';
 import Utils from '../../utils';
 
@@ -13,6 +14,7 @@ const FormComponent = ({
   onChange,
   ...attrs
 }) => {
+  const { hooks } = useHooks();
   const { data, status } = useRefData(component);
   const [ options, setOptions ] = useState([]);
   useEffect(() => {
@@ -30,6 +32,7 @@ const FormComponent = ({
     value: value || '',
     onChange
   }, wrap);
+  }, wrap, hooks.onGetComponent);
 };
 
 FormComponent.propTypes = {
