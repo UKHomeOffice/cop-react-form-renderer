1ecb83f63a6bd55ce503803ce155a857906b64a8
paulfgraham <paul@smartcasual.io>
2022-05-05 14:42:56 +0100

    Add Container component
    Added the Container component, although it's within the `FormComponent.jsx` file to avoid circular dependencies - something that was causing a problem when the Container was in its own file.

--- src/components/FormComponent/FormComponent.jsx
+++ src/components/FormComponent/FormComponent.jsx
@@ -3,13 +3,78 @@ import PropTypes from 'prop-types';
 import { useEffect, useState } from 'react';
 
 // Local imports
-import useHooks from '../../hooks/useHooks';
-import useRefData, { STATUS_COMPLETE } from '../../hooks/useRefData';
+import { STATUS_COMPLETE, useHooks, useRefData } from '../../hooks';
+import { ComponentTypes } from '../../models';
 import Utils from '../../utils';
+
+export const DEFAULT_CONTAINER_CLASS = 'hods-form-container';
+const Container = ({
+  container,
+  value,
+  formData,
+  onChange,
+  wrap,
+  ...attrs
+}) => {
+  const onComponentChange = ({ target }) => {
+    const val = { ...value, [target.name]: target.value };
+    if (typeof onChange === 'function') {
+      onChange({
+        target: {
+          name: container.fieldId,
+          value: val
+        }
+      });
+    }
+  };
+
+  const shouldShow = (options) => {
+    if (options.type === ComponentTypes.CONTAINER) {
+      return Utils.Container.show(options, formData);
+    }
+    return Utils.Component.show(options, formData);
+  };
+
+  return (
+    <div {...attrs} className={DEFAULT_CONTAINER_CLASS} id={container.id}>
+      {container.components && container.components.filter(shouldShow).map((component, index) => {
+        const val = value ? value[component.fieldId] : '';
+        return <FormComponent
+          key={index}
+          component={component}
+          formData={formData}
+          value={val || ''}
+          wrap={wrap}
+          onChange={onComponentChange}
+        />
+      })}
+    </div>
+  );
+};
+
+Container.propTypes = {
+  container: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    fieldId: PropTypes.string.isRequired,
+    components: PropTypes.arrayOf(PropTypes.shape({
+      fieldId: PropTypes.string,
+      type: PropTypes.string
+    }))
+  }).isRequired,
+  value: PropTypes.any,
+  formData: PropTypes.any,
+  wrap: PropTypes.bool,
+  onChange: PropTypes.func
+};
+
+Container.defaultProps = {
+  wrap: true
+};
 
 const FormComponent = ({
   component,
   value,
+  formData,
   wrap,
   onChange,
   ...attrs
@@ -22,6 +87,18 @@ const FormComponent = ({
       setOptions(data);
     }
   }, [component, data, status]);
+
+  if (component.type === ComponentTypes.CONTAINER) {
+    return (
+      <Container
+        container={component}
+        wrap={wrap}
+        onChange={onChange}
+        value={value}
+        formData={formData}
+      />
+    );
+  }
 
   return Utils.Component.get({
     ...attrs,
@@ -36,6 +113,7 @@ const FormComponent = ({
 
 FormComponent.propTypes = {
   component: PropTypes.shape({
+    fieldId: PropTypes.string,
     label: PropTypes.string,
     hint: PropTypes.string,
     data: PropTypes.shape({
@@ -44,6 +122,7 @@ FormComponent.propTypes = {
     })
   }).isRequired,
   value: PropTypes.any,
+  formData: PropTypes.any,
   wrap: PropTypes.bool,
   onChange: PropTypes.func
 };
