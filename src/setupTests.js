f8a98c1a5664a046098e2090d32bfb45c16ba713
Paul Graham <paul@smartcasual.io>
2022-06-08 15:59:00 +0100

    COP-10873: Add validation context (#112)
    Added a `ValidationContext` and a `useValidation` hook to make use of it within the components.

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,3 +1,9 @@
+import { render } from '@testing-library/react';
+import { ValidationContextProvider } from './context';
+
+export const renderWithValidation = (ui, options) => render(
+  <ValidationContextProvider>{ui}</ValidationContextProvider>
+, options);
 
 export const expectObjectLike = (received, expected) => {
   Object.keys(expected).forEach(key => {
