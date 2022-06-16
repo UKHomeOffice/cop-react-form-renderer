f805e29630fac6ff6026720e2f579a2c80fb88b9
George Pemberton <george.pemberton@digital.homeoffice.gov.uk>
2022-06-16 10:11:58 +0100

    added support to pass 'or' type on show when, will evaluate to true if any one of the provided conditions are true

--- src/utils/Condition/index.js
+++ src/utils/Condition/index.js
@@ -1,7 +1,9 @@
 import meetsAllConditions from './meetsAllConditions';
 import meetsCondition from './meetsCondition';
+import meetsOneCondition from './meetsOneCondition';
 
 const Condition = {
+  meetsOne: meetsOneCondition,
   meetsAll: meetsAllConditions,
   met: meetsCondition
 };
