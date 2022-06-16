06ba144c300bb92490fc448aee29c3324c7a43c3
George Pemberton <george.pemberton@digital.homeoffice.gov.uk>
2022-06-16 10:11:58 +0100

    added support to pass 'or' type on show when, will evaluate to true if any one of the provided conditions are true

---
+++ src/utils/Condition/meetsOneCondition.js
@@ -0,0 +1,27 @@
+// Local imports
+import Data from '../Data';
+import meetsCondition from './meetsCondition';
+import setupConditions from './setupConditions';
+
+/**
+ * Evaluates all condition(s) on a page, container, or component.
+ * @param {object} options The container to consider.
+ * @param {object} data The top-level form data.
+ * @returns Boolean true if at least one conditions is met; false otherwise.
+ */
+
+const meetsOneCondition = (options, data) => {
+    console.log("here")
+    const conditions = setupConditions(options);
+    console.log(conditions)
+    if (conditions) {
+        const arr = Array.isArray(conditions) ? conditions: [conditions];
+        return arr.some(condition => {
+            const sourceDataValue = Data.getSource(data, condition.field);
+            return meetsCondition(condition, sourceDataValue);
+        })
+    }
+    return true;
+}
+
+export default meetsOneCondition
