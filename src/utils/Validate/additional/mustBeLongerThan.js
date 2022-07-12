05f85f0f1632de9b2fe126e337a582bb06545cd1
Lewis Clark <lewis.clark@digital.homeoffice.gov.uk>
2022-07-12 14:02:31 +0100

    Made requested changes

--- src/utils/Validate/additional/mustBeLongerThan.js
+++ src/utils/Validate/additional/mustBeLongerThan.js
@@ -1,14 +1,14 @@
 /**
  * Function returns true if the string parameter is longer than a given length, false
- * if it is not. An empty or null string will cause the function to return true - this 
- * is to allow non-required fields to use this kind of validation.
+ * if it is not.
  * @param {string} string - the string to check the length of.
  * @param {number} config.value - the length that string.length must be greater than. 
  * @returns true if string.length is greater than config.value, false if not.
  */
 const mustBeLongerThan = (string, config) => {
-  const str = string || '';
-  if (str === '') {
+  if (!string) {
+    // null, undefined and empty strings should be picked up by the required flag
+    // and not considered here as they would be valid for optional fields.
     return true;
   }
   return (str.length > config.value);
--- src/utils/Validate/additional/mustBeShorterThan.js
+++ src/utils/Validate/additional/mustBeShorterThan.js
@@ -1,13 +1,16 @@
 /**
  * Function returns true if the string parameter is shorter than a given length, false
- * if it is not. An empty or null string will cause the function to return true - this  
- * is to allow non-required fields to use this kind of validation.
+ * if it is not.
  * @param {string} string - the string to check the length of.
  * @param {number} config.value - the length that string.length must be less than. 
  * @returns true if string.length is greater than config.value, false if not.
  */
  const mustBeShorterThan = (string, config) => {
-  const str = string || '';
+  if (!string) {
+    // null, undefined and empty strings should be picked up by the required flag
+    // and not considered here as they would be valid for optional fields.
+    return true;
+  }
   return (str.length < config.value);
 };
 
