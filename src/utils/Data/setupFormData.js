6d21184ff0a79195eeadd04ef79dbbd157f8d8e8
Lewis Clark <lewis.clark@digital.homeoffice.gov.uk>
2022-07-14 17:11:36 +0100

    Added support for default component values

--- src/utils/Data/setupFormData.js
+++ src/utils/Data/setupFormData.js
@@ -1,5 +1,39 @@
+import dayjs from 'dayjs';
+
 // Local imports
 import getSourceData from './getSourceData';
+
+const setDefaultDateValue = (date, data) => {
+  if (date.value === 'today') {
+    data[date.fieldId] = dayjs().format('DD-MM-YYYY');
+  }
+  else {
+    data[date.fieldId] = date.value;
+  }
+}
+
+const setupDefaultValue = (component, data) => {
+  if (!component.value || data[component.fieldId]) {
+    return;
+  }
+  switch (component.type) {
+    case 'date':
+      setDefaultDateValue(component, data);
+      break;
+    default:
+      data[component.fieldId] = component.value;
+  }
+}
+
+const setupDefaultValuesForComponents = (components, data) => {
+  components.forEach(component => setupDefaultValue(component, data));
+}
+
+const setupPageDefaultValues = (pages, data) => {
+  pages.forEach(page => {
+    page.components.filter(c => !c.use).forEach(component => setupDefaultValue(component, data));
+  });
+};
 
 const setupComponentSourceData = (component, data) => {
   if (component.source) {
@@ -8,6 +42,7 @@ const setupComponentSourceData = (component, data) => {
 };
 
 const setupSourceDataForComponents = (components, data) => {
+  console.log(components);
   components.forEach(component => setupComponentSourceData(component, data));
 };
 
@@ -18,8 +53,11 @@ const setupPageSourceData = (pages, data) => {
 };
 
 /**
- * This populates an object with data from source fields.
- * Note that this doesn't currently support sequenced dependencies:
+ * This populates an object with data either from a specified default
+ * value or from a source field. If both are specified, data from a
+ * source field will take priority.
+ * Note that in the case of source fields, this doesn't currently 
+ * support sequenced dependencies:
  *    0: fieldA: source.field = fieldB
  *    1: fieldB: source.field = fieldC
  *    2: fieldC: 'value'
@@ -32,6 +70,8 @@ const setupPageSourceData = (pages, data) => {
  */
 const setupFormData = (pages, components, baseData) => {
   const data = {...baseData};
+  setupDefaultValuesForComponents(components, data);
+  setupPageDefaultValues(pages, data);
   setupSourceDataForComponents(components, data);
   setupPageSourceData(pages, data);
   return data;
